5fafc01b4804c0004d8aaebb2896373008793ec6
Michal Budzowski <michal.budzowski@sonarsource.com>
2024-04-05 15:02:38 +0200

    Add project files.

---
+++ ConsoleApplication68/ConsoleApplication68.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+string func(string* l, bool b1, bool b2, bool b3, bool b4, bool b5, bool b6) {
+    if (b1)
+        if (b2)
+            if (b3)
+                if (b4)
+                    if (b5)
+                        if(b6)
+                            if (l == NULL)
+                                return;
+
+
+
+}
